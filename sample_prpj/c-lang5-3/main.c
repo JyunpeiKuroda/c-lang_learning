01ad874855902931309a39f83c7ef3694dd4d52e
隼平 黒田 <totoro.black6996@gmail.com>
2021-08-27 00:35:38 +0900

    5-3

---
+++ sample_prpj/c-lang5-3/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void main() {
+    char s1[4] = { 'a','b','c','\0' };  //  •¶š—ñ"abc"
+    char s2[] = "HelloWorld.";          //  •¶š—ñ"HelloWorld."
+    char s3[10];                        //  Å‘å10•¶š‚Ü‚Å“ü‚é•¶š—ñ
+    //  •¶š—ñ‚Ì“ü—Í
+    printf("•¶š—ñ‚ğ“ü—Í‚µ‚Ä‚­‚¾‚³‚¢B:");
+    scanf("%s", s3); //  •¶š—ñ‚Ì“ü—Í
+    printf("s1 = %s\n", s1);
+    printf("s2 = %s\n", s2);
+    printf("s3 = %s\n", s3);
+}
