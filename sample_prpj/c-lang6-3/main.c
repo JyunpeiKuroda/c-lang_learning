d8bcc7e3f9c1a783ab377b3cc0500a0a1fe130eb
隼平 黒田 <totoro.black6996@gmail.com>
2021-08-27 09:16:11 +0900

    6-3

---
+++ sample_prpj/c-lang6-3/main.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+//プロトタイプ宣言
+int min(int, int);
+int max(int, int);
+void show(int);
+void line();
+
+void main() {
+    int n1 = 4, n2 = 5;
+    line();
+    show(n1);
+    show(n2);
+    printf("二つの数のうち、大きい数は、%dです。\n", max(n1, n2));
+    printf("二つの数のうち、小さい数は、%dです。\n", min(n1, n2));
+    line();
+}
+
+//  2つの整数のうち最大値を求める関数
+int max(int a, int b)
+{
+    if (a > b) {
+        return a;
+    }
+    return b;
+}
+
+// ２つの整数のうち最小値を求める関数
+int min(int a, int b)
+{
+    if (a < b) {
+        return a;
+    }
+    return b;
+}
+
+//  数値を表示する関数
+void show(int n)
+{
+    printf("数値：%d\n", n);
+    return;
+}
+void line()
+{
+    printf("*********\n");
+}
