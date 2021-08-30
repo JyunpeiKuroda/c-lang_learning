610eee32660b83c1c5426159976e2f7d66172e93
隼平 黒田 <totoro.black6996@gmail.com>
2021-08-30 23:10:01 +0900

    9-3

---
+++ sample_prpj/c-lang9-3/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+//  •Ï”‚Ì’l“ü‚ê‘Ö‚¦‚ğs‚¤ŠÖ”
+void swap(int*, int*);
+
+void main() {
+	int a = 1, b = 2;
+	printf("a = %d b = %d\n", a, b);
+	swap(&a, &b);
+	printf("a = %d b = %d\n", a, b);
+}
+
+//  ’l‚Ì“ü‚ê‘Ö‚¦
+void swap(int* num1, int* num2) {
+	int temp = *num1;
+	*num1 = *num2;
+	*num2 = temp;
+}
